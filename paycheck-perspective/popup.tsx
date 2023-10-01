41ce6866e98a5c2a931b88038713a51ab2336fcb
gabe0313 <gabe0313.quezada@gmail.com>
2023-10-01 02:00:35 -0500

    Added some small UI changes

--- paycheck-perspective/popup.tsx
+++ paycheck-perspective/popup.tsx
@@ -5,19 +5,20 @@ function IndexPopup() {
 
   return (
     <div
+    
       style={{
         display: "flex",
-        flexDirection: "column",
-        padding: 16
+        flexDirection: "row",
+        flexWrap: "wrap",
+        flexGrow: 1,
+        padding: 16,
+        backgroundColor: "#f5e6f2"
       }}>
+      
       <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
+        PaycheckPerspective
       </h2>
+      <p> Here to aid you in your job searching needs </p>
       <input onChange={(e) => setData(e.target.value)} value={data} />
       <a href="https://docs.plasmo.com" target="_blank">
         View Docs
