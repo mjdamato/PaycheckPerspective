76bb6371c08483ec16d0ca996efcc5cbcc6fc2e3
gabe0313 <gabe0313.quezada@gmail.com>
2023-10-16 14:51:02 -0500

    Changes to the scraper

--- paycheck-perspective/scraper/src/index.ts
+++ paycheck-perspective/scraper/src/index.ts
@@ -1,15 +1,12 @@
 import axios from 'axios';
 import * as cheerio from "cheerio";
 
-const url = 'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1'; // URL we're scraping
+const url = ''; // URL we're scraping
 const AxiosInstance = axios.create(); // Create a new Axios Instance
 
 // This is the structure of the player data we recieve
 interface PlayerData {
-  rank: number; // 1 - 20 rank
-  name: string;
-  nationality: string;
-  goals: number;
+  //create it with the values to display it
 }
 
 // Send an async HTTP Get request to the url
@@ -18,23 +15,7 @@ AxiosInstance.get(url)
     response => {
       const html = response.data; // Get the HTML from the HTTP request
       const $ = cheerio.load(html); // Load the HTML string into cheerio
-      const statsTable: cheerio.Cheerio = $('.statsTableContainer > tr'); // Parse the HTML and extract just whatever code contains .statsTableContainer and has tr inside
-      const topScorers: PlayerData[] = [];
-
-      statsTable.each((i, elem) => {
-        const rank: number = parseInt($(elem).find('.rank > strong').text()); // Parse the rank
-        const name: string = $(elem).find('.playerName > strong').text(); // Parse the name
-        const nationality: string = $(elem).find('.playerCountry').text(); // Parse the country
-        const goals: number = parseInt($(elem).find('.mainStat').text()); // Parse the number of goals
-        topScorers.push({
-          rank,
-          name,
-          nationality,
-          goals
-        })
-      })
-
-      console.log(topScorers);
+    
     }
   )
   .catch(console.error); // Error handling
