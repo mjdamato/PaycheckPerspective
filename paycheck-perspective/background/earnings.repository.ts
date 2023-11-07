53ab1596e921f9e9b4c84c0c56f99ac25f85384a
taylorcassidy <99693486+taylorcassidy@users.noreply.github.com>
2023-11-07 12:19:18 -0600

    Update earnings.repository.ts

--- paycheck-perspective/background/earnings.repository.ts
+++ paycheck-perspective/background/earnings.repository.ts
@@ -1,11 +1,11 @@
 /*
 import connection from "../db";
-import Tutorial from "../earnings.model";
+import Earnings from "../earnings.model";
 
 interface IEarningsRepository {
   save(earnings: Earnings): Promise<Earnings>;
-  retrieveAll(searchParams: {occupation: string, median_earnings: number}): Promise<Tutorial[]>;
-  retrieveById(earningsoccupation: string): Promise<Tutorial | undefined>;
+  retrieveAll(searchParams: {occupation: string, median_earnings: number}): Promise<Earnings[]>;
+  retrieveById(earningsoccupation: string): Promise<Earnings | undefined>;
   update(earnings: new): Promise<number>;
   delete(occupation: string): Promise<number>;
   deleteAll(): Promise<number>;
@@ -30,13 +30,13 @@ import { OkPacket } from "mysql2";
 save(earnings: Earnings): Promise<Earnings> {
   return new Promise((resolve, reject) => {
     connection.query<OkPacket>(
-      "INSERT INTO earnings (occupation,Number_of_full_time_workers,Number_of_men, Number_of_women, Percentage_of_women_in_occupational_group, Median_earnings) VALUES(?,?,?)",
-      //[tutorial.title, tutorial.description, tutorial.published ? tutorial.published : false],
+      "INSERT INTO earnings (occupation, Number_of_full_time_workers, Number_of_men, Number_of_women, Percentage_of_women_in_occupational_group, Median_earnings) VALUES(?,?,?)",
+      //[earnings.occupation, earnings.Median_earnings, earnings.published ? earnings.published : false],
       (err, res) => {
         if (err) reject(err);
         else
           this.retrieveById(res.insertId)
-            .then((tutorial) => resolve(tutorial!))
+            .then((earnings) => resolve(earnings!))
             .catch(reject);
       }
     );
@@ -66,7 +66,7 @@ retrieveAll(searchParams: {occupation?: string, median_earnings?: number}): Prom
 
   retrieveById(earningsOccupation: string): Promise<Earnings> {
     return new Promise((resolve, reject) => {
-      connection.query<Tutorial[]>(
+      connection.query<Earnings[]>(
         "SELECT * FROM earnings WHERE occupation = ?",
         [earningsOccupation],
         (err, res) => {
@@ -83,7 +83,7 @@ update(earnings: Earnings): Promise<number> {
   return new Promise((resolve, reject) => {
     connection.query<OkPacket>(
       "UPDATE earnings SET median_earnings = ?, occupation = ?",
-      [tutorial.title, tutorial.description, tutorial.published, tutorial.id],
+      [earnings.occupation, earnings.Median_earnings, earnings.published,],
       (err, res) => {
         if (err) reject(err);
         else resolve(res.affectedRows);
@@ -97,8 +97,8 @@ import { OkPacket } from "mysql2";
 delete(EarningsRepository: number): Promise<number> {
   return new Promise((resolve, reject) => {
     connection.query<OkPacket>(
-      "DELETE FROM tutorials WHERE id = ?",
-      [tutorialId],
+      "DELETE FROM earnings WHERE id = ?",
+      [earningsId],
       (err, res) => {
         if (err) reject(err);
         else resolve(res.affectedRows);
